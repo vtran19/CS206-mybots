d2a2127397372c65a736df0b78e7c6cc38f62355
vtran19 <vtran1919@gmail.com>
2023-01-25 14:38:37 -0500

    added gravity forces and floor to the simulation

--- simulate.py
+++ simulate.py
@@ -1,11 +1,15 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
 
 p.loadSDF("box.sdf")
 for x in range(1001):
-    time.sleep(1/90)
+    time.sleep(1/100)
     p.stepSimulation()
     print(x)
 
