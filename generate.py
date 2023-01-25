2fbe6ec4805fccb7da9106da5e8eca1629d80854
vtran19 <vtran1919@gmail.com>
2023-01-25 15:01:45 -0500

    changed size successfully

--- generate.py
+++ generate.py
@@ -1,6 +1,8 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+length, width, height = 1, 2, 3
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
 
 pyrosim.End()
