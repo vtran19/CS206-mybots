65ef164c9bdc93434e64010673e206973a98cc30
vtran19 <vtran1919@gmail.com>
2023-01-30 13:30:12 -0500

    box on the floor

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@ import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("box.sdf")
 
 length, width, height = 1, 2, 3
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
+x, y, z = 0, 0, 1.5
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
 
 pyrosim.End()
