dbe7acadc1f5e6c627131a34d3a852481cc134a8
vtran19 <vtran1919@gmail.com>
2023-01-30 22:18:44 -0500

    three link, 2 joint

--- generate.py
+++ generate.py
@@ -10,7 +10,7 @@ def Create_World():
 
     pyrosim.End()
 
-def Create_Robot():
+def Create_Arch_Robot():
     pyrosim.Start_URDF("body.urdf")
 
     length, width, height = 1, 1, 1
@@ -38,5 +38,20 @@ def Create_Robot():
 
     pyrosim.End()
 
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+
+    length, width, height = 1, 1, 1
+
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[length, width, height])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[length, width, height])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[length, width, height])
+
+    pyrosim.End()
+
+
 Create_World()
+#Create_Arch_Robot()
 Create_Robot()
