0be9c115083ba32f3207f486ae712507b0b84df9
vtran19 <vtran1919@gmail.com>
2023-01-30 20:45:28 -0500

    defining robot

--- generate.py
+++ generate.py
@@ -1,10 +1,24 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
 
-length, width, height = 1, 1, 1
-x, y, z = 0, 0, 0.5
+    length, width, height = 1, 1, 1
+    x, y, z = -2, 2, 0.5
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
 
-pyrosim.End()
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+
+    length, width, height = 1, 1, 1
+    x, y, z = 0, 0, 0.5
+
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length, width, height])
+
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
