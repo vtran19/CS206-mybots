d14570325cc76395b100bb32c01f11721c62dec4
vtran19 <vtran1919@gmail.com>
2023-02-13 12:31:55 -0500

    found a good sextet for movement

--- simulate.py
+++ simulate.py
@@ -10,13 +10,13 @@ import random
 PI = math.pi
 ITERATIONS = 1000
 
-amplitude_FrontLeg = PI/2
-frequency_FrontLeg = 3
+amplitude_FrontLeg = PI/6
+frequency_FrontLeg = 6
 phaseOffset_FrontLeg = 0
 
-amplitude_BackLeg = PI/2
-frequency_BackLeg = 3
-phaseOffset_BackLeg = 0
+amplitude_BackLeg = PI/8
+frequency_BackLeg = 6
+phaseOffset_BackLeg = PI/2
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
