906238c892c2b6469430d27f61fe5e67e3e0de3a
vtran19 <vtran1919@gmail.com>
2023-02-22 13:29:20 -0500

    generating a brain

--- generate.py
+++ generate.py
@@ -10,35 +10,7 @@ def Create_World():
 
     pyrosim.End()
 
-def Create_Arch_Robot():
-    pyrosim.Start_URDF("body.urdf")
-
-    length, width, height = 1, 1, 1
-    x, y, z = 0, 0, 0.5
-
-    pyrosim.Send_Cube(name="Link0", pos=[x,y,z] , size=[length, width, height])
-    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
-
-    pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5] , size=[length, width, height])
-    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0,0,1])
-    
-    pyrosim.Send_Cube(name="Link2", pos=[0,0,0.5] , size=[length, width, height])
-    pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0,0.5,0.5])
-
-    pyrosim.Send_Cube(name="Link3", pos=[0,0.5,0] , size=[length, width, height])
-    pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0,1,0])
-
-    pyrosim.Send_Cube(name="Link4", pos=[0,0.5,0] , size=[length, width, height])
-    pyrosim.Send_Joint( name = "Link4_Link5" , parent= "Link4" , child = "Link5" , type = "revolute", position = [0,0.5,-0.5])
-
-    pyrosim.Send_Cube(name="Link5", pos=[0,0,-0.5] , size=[length, width, height])
-    pyrosim.Send_Joint( name = "Link5_Link6" , parent= "Link5" , child = "Link6" , type = "revolute", position = [0,0,-1])
-
-    pyrosim.Send_Cube(name="Link6", pos=[0,0,-0.5] , size=[length, width, height])
-
-    pyrosim.End()
-
-def Create_Robot():
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
 
     length, width, height = 1, 1, 1
@@ -51,7 +23,11 @@ def Create_Robot():
 
     pyrosim.End()
 
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+    pyrosim.End()
 
 Create_World()
-#Create_Arch_Robot()
-Create_Robot()
+Generate_Body()
+Generate_Brain()
