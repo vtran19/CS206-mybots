5e9a6ea54625022baf375d4682d38207d9d6ffcc
vtran19 <vtran1919@gmail.com>
2023-02-27 00:01:39 -0500

    actually finished, removed unused methods

--- motor.py
+++ motor.py
@@ -7,17 +7,6 @@ class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
         self.motorValues = numpy.zeros(c.ITERATIONS)
-        self.Prepare_To_Act()
-
-    def Prepare_To_Act(self):
-        self.amplitude = c.AMPLITUDE
-        self.phaseOffset = c.PHASE_OFFSET
-        self.frequency = c.FREQUENCY
-
-        if self.jointName == "Torso_FrontLeg":
-            self.motorValues = self.amplitude * numpy.sin(self.frequency/2 * numpy.linspace(0, 2*c.PI, c.ITERATIONS) + self.phaseOffset)
-        else:
-            self.motorValues = self.amplitude * numpy.sin(self.frequency * numpy.linspace(0, 2*c.PI, c.ITERATIONS) + self.phaseOffset)
 
     def Set_Value(self, robotId, desiredAngle):
         pyrosim.Set_Motor_For_Joint(
@@ -26,7 +15,4 @@ class MOTOR:
             controlMode = p.POSITION_CONTROL,
             targetPosition = desiredAngle,
             maxForce = c.MAX_FORCE)
-
-    def Save_Value(self):
-        numpy.save("data/targetAngles_" + self.jointName, self.motorValues)
         
--- robot.py
+++ robot.py
@@ -32,9 +32,7 @@ class ROBOT:
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
                 desiredAngle = self.nn.Get_Value_Of(neuronName)
                 self.motors[jointName].Set_Value(self.robotId, desiredAngle)
-                print(neuronName, jointName, desiredAngle)
-        # for motor in self.motors:
-        #     self.motors[motor].Set_Value(self.robotId, t)
+        
     
     def Think(self):
         self.nn.Update()
